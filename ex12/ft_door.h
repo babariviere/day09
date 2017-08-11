7d7f0180d450378bfc8169cdf7c2f02828561b7f
Bastien RIVIERE <briviere@e2r11p18.42.fr>
2017-08-11 05:59:19 +0200

    Add ex12

---
+++ ex12/ft_door.h
@@ -0,0 +1,23 @@
+
+#ifndef FT_DOOR_H
+# define FT_DOOR_H
+
+# include <unistd.h>
+
+# define OPEN	0
+# define CLOSE	1
+# define TRUE	1
+
+typedef struct	s_door
+{
+	int state;
+}				t_door;
+
+typedef int		ft_bool;
+
+void			open_door(t_door *door);
+void			close_door(t_door *door);
+ft_bool			is_door_open(t_door *door);
+ft_bool			is_door_close(t_door *door);
+
+#endif
