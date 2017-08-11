c5c6eec72bf2caedb1a450abf5213167a0fa389f
Bastien RIVIERE <briviere@e2r11p18.42.fr>
2017-08-11 02:04:12 +0200

    Fix

--- ex09/where_am_i_baba.sh
+++ ex09/where_am_i_baba.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-ifconfig | sed -n 's/inet \([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\) .*/\1/p' | awk '{$1=$1};1'
+INET=`ifconfig | sed -n 's/inet \([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\) .*/\1/p' | awk '{$1=$1};1'`
+
+if [ -z "$INET" ]; then
+	echo "Je suis perdu!"
+else
+	for addr in $INET; do
+		echo $addr
+	done
+fi
